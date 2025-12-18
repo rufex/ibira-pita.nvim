e97fee2d2c24a8d5f31aa82d157b66e32f793efa
rufex <rufex.dev@gmail.com>
2025-12-18 11:30:37 +0100

    feat: reapply colors
    More orange and purple and less yellow

--- lua/ibira-pita/init.lua
+++ lua/ibira-pita/init.lua
@@ -11,31 +11,31 @@ local function apply_colors()
   hl("Comment", { fg = palette.grey, italic = true })
   hl("Constant", { fg = palette.yellow_light })
   hl("String", { fg = palette.yellow_strong })
-  hl("Character", { fg = palette.orange })
+  hl("Character", { fg = palette.yellow_strong })
   hl("Number", { fg = palette.yellow })
-  hl("Boolean", { fg = palette.yellow_strong })
+  hl("Boolean", { fg = palette.yellow })
   hl("Float", { fg = palette.yellow })
   hl("Identifier", { fg = palette.yellow_pale })
-  hl("Function", { fg = palette.yellow_light })
-  hl("Statement", { fg = palette.yellow_strong })
-  hl("Conditional", { fg = palette.yellow })
-  hl("Repeat", { fg = palette.yellow })
-  hl("Label", { fg = palette.yellow })
-  hl("Operator", { fg = palette.purple })
-  hl("Keyword", { fg = palette.grey_dark })
-  hl("Exception", { fg = palette.yellow })
-  hl("PreProc", { fg = palette.brown_orange })
-  hl("Include", { fg = palette.brown_orange })
-  hl("Define", { fg = palette.brown_orange })
-  hl("Macro", { fg = palette.brown_orange })
-  hl("PreCondit", { fg = palette.brown_orange })
-  hl("Type", { fg = palette.yellow_light })
-  hl("StorageClass", { fg = palette.yellow_light })
-  hl("Structure", { fg = palette.beige })
+  hl("Function", { fg = palette.brown_orange })
+  hl("Statement", { fg = palette.purple })
+  hl("Conditional", { fg = palette.purple })
+  hl("Repeat", { fg = palette.purple })
+  hl("Label", { fg = palette.purple_light })
+  hl("Operator", { fg = palette.purple_light })
+  hl("Keyword", { fg = palette.purple_dark })
+  hl("Exception", { fg = palette.purple })
+  hl("PreProc", { fg = palette.purple_darker })
+  hl("Include", { fg = palette.purple_darker })
+  hl("Define", { fg = palette.purple_darker })
+  hl("Macro", { fg = palette.purple_darker })
+  hl("PreCondit", { fg = palette.purple_darker })
+  hl("Type", { fg = palette.brown_orange_dark })
+  hl("StorageClass", { fg = palette.purple_dark })
+  hl("Structure", { fg = palette.brown_orange_dark })
   hl("Substitute", { fg = palette.black, bg = palette.brown_orange })
-  hl("Typedef", { fg = palette.yellow_light })
-  hl("Special", { fg = palette.purple })
-  hl("SpecialChar", { fg = palette.purple })
+  hl("Typedef", { fg = palette.brown_orange_dark })
+  hl("Special", { fg = palette.orange })
+  hl("SpecialChar", { fg = palette.orange })
   hl("Tag", { fg = palette.orange })
   hl("Delimiter", { fg = palette.grey_light })
   hl("SpecialComment", { fg = palette.grey_light })
@@ -43,9 +43,9 @@ local function apply_colors()
   hl("Underlined", { underline = true })
   hl("Ignore", { fg = palette.grey })
   hl("Error", { fg = palette.red })
-  hl("Todo", { fg = palette.yellow_pale, bg = palette.grey_dark, bold = true })
+  hl("Todo", { fg = palette.orange, bg = palette.grey_dark, bold = true })
   hl("CursorLine", { bg = palette.black_light })
-  hl("CursorLineNr", { fg = palette.yellow, bold = true })
+  hl("CursorLineNr", { fg = palette.brown_orange, bold = true })
   hl("LineNr", { fg = palette.grey })
   hl("SignColumn", { fg = palette.grey_light, bg = palette.black })
   hl("StatusLine", { fg = palette.black, bg = palette.brown_orange })
@@ -53,58 +53,58 @@ local function apply_colors()
   hl("Visual", { bg = palette.grey_darker })
   hl("NormalFloat", { fg = palette.beige, bg = palette.black }) -- Same As Normal
   hl("Pmenu", { fg = palette.beige, bg = palette.black }) -- Same as Normal
-  hl("PmenuSel", { fg = palette.black, bg = palette.yellow })
+  hl("PmenuSel", { fg = palette.black, bg = palette.brown_orange })
   hl("PmenuSbar", { bg = palette.grey_dark })
   hl("PmenuThumb", { bg = palette.grey })
-  hl("TabLine", { fg = palette.yellow_pale, bg = palette.black })
+  hl("TabLine", { fg = palette.grey_light, bg = palette.black })
   hl("TabLineFill", { fg = palette.black, bg = palette.grey_dark })
-  hl("TabLineSel", { fg = palette.yellow_light, bg = palette.grey_dark })
-  hl("DiffAdd", { fg = palette.green, bg = palette.black })
-  hl("DiffChange", { fg = palette.beige, bg = palette.black })
+  hl("TabLineSel", { fg = palette.brown_orange, bg = palette.grey_dark })
+  hl("DiffAdd", { fg = palette.yellow_pale, bg = palette.black })
+  hl("DiffChange", { fg = palette.blue, bg = palette.black })
   hl("DiffDelete", { fg = palette.red, bg = palette.black })
   hl("DiffText", { fg = palette.black, bg = palette.green, bold = true })
   hl("DiagnosticError", { fg = palette.red })
   hl("DiagnosticWarn", { fg = palette.yellow })
   hl("DiagnosticInfo", { fg = palette.grey_dark })
   hl("DiagnosticHint", { fg = palette.grey })
-  hl("TSAnnotation", { fg = palette.grey_light })
-  hl("TSAttribute", { fg = palette.yellow_light })
+  hl("TSAnnotation", { fg = palette.purple_light })
+  hl("TSAttribute", { fg = palette.pale_brown })
   hl("TSBoolean", { fg = palette.yellow })
-  hl("TSCharacter", { fg = palette.orange })
+  hl("TSCharacter", { fg = palette.yellow_strong })
   hl("TSComment", { fg = palette.grey, italic = true })
-  hl("TSConditional", { fg = palette.yellow })
-  hl("TSConstant", { fg = palette.yellow })
-  hl("TSConstBuiltin", { fg = palette.yellow_pale })
+  hl("TSConditional", { fg = palette.purple })
+  hl("TSConstant", { fg = palette.yellow_light })
+  hl("TSConstBuiltin", { fg = palette.yellow_light })
   hl("TSConstMacro", { fg = palette.yellow_light })
   hl("TSError", { fg = palette.red })
-  hl("TSException", { fg = palette.yellow })
-  hl("TSField", { fg = palette.yellow_pale })
+  hl("TSException", { fg = palette.purple })
+  hl("TSField", { fg = palette.pale_brown })
   hl("TSFloat", { fg = palette.yellow })
-  hl("TSFunction", { fg = palette.yellow_light })
-  hl("TSFuncBuiltin", { fg = palette.yellow_pale })
-  hl("TSFuncMacro", { fg = palette.yellow })
-  hl("TSKeyword", { fg = palette.yellow })
-  hl("TSKeywordFunction", { fg = palette.yellow })
-  hl("TSLabel", { fg = palette.yellow })
-  hl("TSMethod", { fg = palette.yellow_light })
-  hl("TSNamespace", { fg = palette.grey })
+  hl("TSFunction", { fg = palette.brown_orange })
+  hl("TSFuncBuiltin", { fg = palette.brown_orange })
+  hl("TSFuncMacro", { fg = palette.brown_orange })
+  hl("TSKeyword", { fg = palette.purple_dark })
+  hl("TSKeywordFunction", { fg = palette.purple_dark })
+  hl("TSLabel", { fg = palette.purple_light })
+  hl("TSMethod", { fg = palette.brown_orange })
+  hl("TSNamespace", { fg = palette.brown_orange_dark })
   hl("TSNumber", { fg = palette.yellow })
-  hl("TSOperator", { fg = palette.purple })
-  hl("TSParameter", { fg = palette.yellow })
-  hl("TSProperty", { fg = palette.yellow })
-  hl("TSPunctDelimiter", { fg = palette.grey_dark })
-  hl("TSPunctBracket", { fg = palette.grey_dark })
+  hl("TSOperator", { fg = palette.purple_light })
+  hl("TSParameter", { fg = palette.yellow_pale })
+  hl("TSProperty", { fg = palette.pale_brown })
+  hl("TSPunctDelimiter", { fg = palette.grey_light })
+  hl("TSPunctBracket", { fg = palette.grey_light })
   hl("TSPunctSpecial", { fg = palette.grey_dark })
-  hl("TSString", { fg = palette.orange })
-  hl("TSStringRegex", { fg = palette.purple })
-  hl("TSStringEscape", { fg = palette.purple })
-  hl("TSSymbol", { fg = palette.yellow })
-  hl("TSType", { fg = palette.yellow })
-  hl("TSTypeBuiltin", { fg = palette.yellow })
+  hl("TSString", { fg = palette.yellow_strong })
+  hl("TSStringRegex", { fg = palette.orange })
+  hl("TSStringEscape", { fg = palette.orange })
+  hl("TSSymbol", { fg = palette.orange })
+  hl("TSType", { fg = palette.brown_orange_dark })
+  hl("TSTypeBuiltin", { fg = palette.brown_orange_dark })
   hl("TSVariable", { fg = palette.yellow_pale })
-  hl("TSVariableBuiltin", { fg = palette.yellow })
-  hl("Title", { fg = palette.yellow_strong })
-  hl("@variable", { fg = palette.red })
+  hl("TSVariableBuiltin", { fg = palette.orange })
+  hl("Title", { fg = palette.brown_orange })
+  hl("@variable", { fg = palette.yellow_pale })
   hl("Directory", { fg = palette.orange })
   hl("SignAdd", { fg = palette.yellow_pale })
   hl("SignChange", { fg = palette.blue })
@@ -119,63 +119,63 @@ local function apply_colors()
   hl("FoldColumn", { fg = palette.grey, bg = palette.black })
   hl("ErrorMsg", { fg = palette.red, bold = true })
   hl("WarningMsg", { fg = palette.yellow, bold = true })
-  hl("MoreMsg", { fg = palette.yellow_pale })
-  hl("Question", { fg = palette.yellow_light })
+  hl("MoreMsg", { fg = palette.brown_orange })
+  hl("Question", { fg = palette.brown_orange })
 
-  hl("GitSignsAdd", { fg = palette.purple })
-  hl("GitSignsChange", { fg = palette.purple })
-  hl("GitSignsDelete", { fg = palette.purple })
-  hl("GitSignsChangeDelete", { fg = palette.purple })
+  hl("GitSignsAdd", { fg = palette.green })
+  hl("GitSignsChange", { fg = palette.yellow })
+  hl("GitSignsDelete", { fg = palette.red })
+  hl("GitSignsChangeDelete", { fg = palette.red })
 
   hl("TelescopeNormal", { fg = palette.beige, bg = palette.black })
   hl("TelescopeBorder", { fg = palette.grey_darker, bg = palette.black })
-  hl("TelescopeSelection", { fg = palette.yellow, bg = palette.grey_darker })
+  hl("TelescopeSelection", { fg = palette.brown_orange, bg = palette.grey_darker })
 
   hl("TreesitterContext", { bg = palette.black_light })
 
   hl("LspReferenceText", { bg = palette.grey_darker })
   hl("LspReferenceRead", { bg = palette.grey_darker })
   hl("LspReferenceWrite", { bg = palette.grey_darker })
-  hl("LspSignatureActiveParameter", { fg = palette.yellow_strong })
+  hl("LspSignatureActiveParameter", { fg = palette.orange })
 
   -- Ruby
-  hl("@variable.member.ruby", { fg = palette.purple })
-  hl("@string.ruby", { fg = palette.orange })
-  hl("@constant.ruby", { fg = palette.purple })
-  hl("@type.ruby", { fg = palette.pale_brown })
+  hl("@variable.member.ruby", { fg = palette.yellow_pale })
+  hl("@string.ruby", { fg = palette.yellow_strong })
+  hl("@constant.ruby", { fg = palette.yellow_light })
+  hl("@type.ruby", { fg = palette.purple_light })
 
   -- Vue.js specific highlights
-  hl("vueComponentName", { fg = palette.yellow_light }) -- Component names
-  hl("vueTag", { fg = palette.yellow_pale }) -- HTML tags
+  hl("vueComponentName", { fg = palette.brown_orange_dark }) -- Component names
+  hl("vueTag", { fg = palette.brown_orange_dark }) -- HTML tags
   hl("vueDirective", { fg = palette.purple }) -- v-if, v-for, etc
-  hl("vueAttr", { fg = palette.yellow }) -- Component props/attributes
-  hl("vueMethod", { fg = palette.yellow_light }) -- Methods
-  hl("vueComputed", { fg = palette.yellow_light }) -- Computed properties
-  hl("vueWatch", { fg = palette.yellow_light }) -- Watch properties
-  hl("vueEmit", { fg = palette.yellow_pale }) -- Emits
-  hl("vueProps", { fg = palette.yellow }) -- Props definitions
+  hl("vueAttr", { fg = palette.pale_brown }) -- Component props/attributes
+  hl("vueMethod", { fg = palette.brown_orange }) -- Methods
+  hl("vueComputed", { fg = palette.brown_orange }) -- Computed properties
+  hl("vueWatch", { fg = palette.brown_orange }) -- Watch properties
+  hl("vueEmit", { fg = palette.orange }) -- Emits
+  hl("vueProps", { fg = palette.pale_brown }) -- Props definitions
   hl("vueData", { fg = palette.yellow_pale }) -- Data properties
-  hl("vueFilter", { fg = palette.purple }) -- Filters
+  hl("vueFilter", { fg = palette.purple_light }) -- Filters
   hl("vueInterpolation", { fg = palette.yellow_strong }) -- {{ interpolation }}
   hl("vueTemplate", { fg = palette.grey_light }) -- <template> tag
   hl("vueScript", { fg = palette.grey_light }) -- <script> tag
   hl("vueStyle", { fg = palette.grey_light }) -- <style> tag
-  hl("vueTemplateLiteral", { fg = palette.orange })
-  hl("vueSetupKeyword", { fg = palette.purple }) -- 'setup' keyword
-  hl("vueRef", { fg = palette.yellow_pale }) -- ref declarations
-  hl("vueReactive", { fg = palette.yellow_pale }) -- reactive declarations
-  hl("vueComposable", { fg = palette.yellow_light }) -- composable functions
+  hl("vueTemplateLiteral", { fg = palette.yellow_strong })
+  hl("vueSetupKeyword", { fg = palette.purple_dark }) -- 'setup' keyword
+  hl("vueRef", { fg = palette.orange }) -- ref declarations
+  hl("vueReactive", { fg = palette.orange }) -- reactive declarations
+  hl("vueComposable", { fg = palette.brown_orange }) -- composable functions
   hl("vueLifecycleHook", { fg = palette.purple }) -- onMounted, etc
-  hl("vueRouter", { fg = palette.yellow_light }) -- Router related code
-  hl("vueStore", { fg = palette.yellow_light }) -- Store related code
-  hl("vueTypeScript", { fg = palette.yellow }) -- TypeScript annotations
-  hl("vueInterface", { fg = palette.yellow_light }) -- Interfaces
-  hl("vueType", { fg = palette.yellow_light }) -- Type definitions
-  hl("@vue.component", { fg = palette.yellow_light })
+  hl("vueRouter", { fg = palette.brown_orange_dark }) -- Router related code
+  hl("vueStore", { fg = palette.brown_orange_dark }) -- Store related code
+  hl("vueTypeScript", { fg = palette.brown_orange_dark }) -- TypeScript annotations
+  hl("vueInterface", { fg = palette.brown_orange_dark }) -- Interfaces
+  hl("vueType", { fg = palette.brown_orange_dark }) -- Type definitions
+  hl("@vue.component", { fg = palette.brown_orange_dark })
   hl("@vue.directive", { fg = palette.purple })
   hl("@vue.expression", { fg = palette.yellow_strong })
   hl("@vue.interpolation", { fg = palette.yellow_strong })
-  hl("@vue.script_setup", { fg = palette.purple })
+  hl("@vue.script_setup", { fg = palette.purple_dark })
   hl("@vue.template_tag", { fg = palette.grey_light })
 
   -- Mini.Tabline
